ec0005d18170c324994cd9410557f42d00bab0cb
pscorp007 <pscorp007@gmail.com>
2020-10-14 04:39:45 +0530

    Creation and removal of Custom lists adding/removing problems to/from custom lists all of these inside profile routes plus some css changes

--- public/scripts/master.js
+++ public/scripts/master.js
@@ -2,3 +2,23 @@
 var x = document.getElementsByTagName("nav");
 x[0].onmouseover = function() {var y = document.getElementsByTagName("main"); y[0].className = "main-class";}
 x[0].onmouseout = function() {var y = document.getElementsByTagName("main"); y[0].className = "main";}
+
+
+$(document).ready(function() {
+    $('.show-userlists').on('click', function(e) {
+        // alert('clicked');
+        var cardContent   = $(this).closest('.card-content');
+        var cardContentID = $(cardContent).attr('id');
+        // alert(cardContentID);
+        $('#' + cardContentID).css("display","none");
+        $('#' + 'back' + cardContentID.substr(5,cardContentID.length)).css("display","block");
+    })
+    $('.modal-footer').on('click', function(e) {
+        // alert('clicked');
+        var cardContent   = $(this).closest('.card-content');
+        var cardContentID = $(cardContent).attr('id');
+        // alert(cardContentID);
+        $('#' + cardContentID).css("display","none");
+        $('#' + 'front' + cardContentID.substr(4,cardContentID.length)).css("display","block");
+    })
+});
